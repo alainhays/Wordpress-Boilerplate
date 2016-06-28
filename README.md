d6f83eb33df45154c22dc42d6ea32714f6bb6113
Jamie Throup <jt@wearelittle.agency>
2016-06-28 09:29:34 +0100

    Near final folder structure and grid implemented

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Wordpress-Boilerplate
+
+##Introduction
+
 Wordpress Boilerplate using Stylus, GULP, HTML5 and CSS3
+
+###Setup
+
+Simply clone or download the repository, unzip, and navigation to the 'default' theme folder and install via Terminal and Command Line
--- wp-content/themes/default/src/templates/index.php
+++ wp-content/themes/default/src/templates/index.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
 
-<h1>Hello world</h1>
-
 <section class="test-area">
+
     <div class="row">
-        <div class="span4">First Block</div>
-        <div class="span4">Second Block</div>
-        <div class="span4">Third Block</div>
+
+        <div class="span12">Hello world</div>
+
     </div>
 
 </section>
