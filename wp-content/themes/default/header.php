be22df8ae6703b1baae0c0ba8cb9092d9046adbf
Jamie Throup <jt@wearelittle.agency>
2016-08-08 11:12:24 +0100

    Changes to order

---
+++ wp-content/themes/default/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en-GB">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<?php
+		$pageTitle = wp_title( '', false );
+		$pageTitle .= ( wp_title( '', false ) ) ? ' : ' : '';
+		$pageTitle .= get_bloginfo( 'name' );
+		$pageTitle = trim( $pageTitle );
+		?>
+		<title><?=$pageTitle?></title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php wp_head(); ?>
+		<link rel="stylesheet" href="<?=get_template_directory_uri()?>/assets/css/main.css">
+
+		<!--[if lt IE 9]>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.3.1/es5-shim.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.3.1/es5-sham.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<!--<div class="hidden"><img src="<?=get_template_directory_uri()?>/assets/images/icons.svg" inline></div>-->
---
+++ wp-content/themes/default/index.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<section class="test-area">
+
+    <div class="row">
+
+        <div class="span12">Hello world</div>
+
+    </div>
+
+</section>
+
+<?php get_footer(); ?>
